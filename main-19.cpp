052d27b69cbc502b49f4f5d7ba584bf9a0235e43
Vrey415-lang <vrey415@uan.edu.co>
2026-09-17 19:50:45 -0500

    Add files via upload

---
+++ main-19.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() 
+{
+    float num1, num2, suma;
+
+    cout << "Ingrese el primer número decimal: ";
+    cin >> num1;
+    cout << "Ingrese el segundo número decimal: ";
+    cin >> num2;
+
+    suma = num1 + num2;
+
+    cout << fixed << setprecision(2);
+    cout << setw(10) << num1 << endl;
+    cout << setw(10) << num2 << endl;
+    cout << setw(10) << suma << endl;
+
+    return 0;
+}
